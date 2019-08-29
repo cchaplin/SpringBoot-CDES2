ec5836b2578e974c2db6b3a5d7c149e26b19fc8c
Senthil Raja Chermapandian <senthilraja.chermapandian@cognizant.com>
2019-08-29 11:38:54 +0000

    added eureka server and discovery client

--- otms/src/main/java/com/s2/otms/OtmsApplication.java
+++ otms/src/main/java/com/s2/otms/OtmsApplication.java
@@ -2,7 +2,9 @@ package com.s2.otms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OtmsApplication {
 
